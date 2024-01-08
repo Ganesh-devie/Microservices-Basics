81a2c27fd6c39732579bbf191b84956006b9e1d5
Ganesh-devie <118337110+Ganesh-devie@users.noreply.github.com>
2024-01-08 22:31:33 +0530

    init Ms-movie-rating

---
+++ Ms-movie-rating/src/main/java/com/example/Msmovierating/MsMovieRatingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Msmovierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMovieRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMovieRatingApplication.class, args);
+	}
+
+}
---
+++ Ms-movie-rating/src/test/java/com/example/Msmovierating/MsMovieRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Msmovierating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMovieRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
